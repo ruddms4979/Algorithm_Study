faeaa2ee847bf7756cc01add1a5ff54217d944a5
ruddms4979 <ruddms4979@gmail.com>
2022-04-14 17:52:14 +0900

    a

--- 2주차_이취코_greedy/chap11_4.cpp
+++ 2주차_이취코_greedy/chap11_4.cpp
@@ -1,6 +1,30 @@
-//
+// 볼링공 고르기  다른방법 확인해보기!!!!!
 
 #include <stdio.h>
 #include <iostream>
 #include <vector>
 using namespace std;
+vector<int> arr;
+
+
+int main(){
+    
+    int n,m;
+    cin >> n >> m;
+
+    for(int i=0; i<n; i++){
+        int a;
+        cin >> a;
+        arr.push_back(a);
+    }
+    
+    int count=0;
+
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(arr[i]!=arr[j]) count += 1;
+        }
+    }
+    cout << count;
+
+}
